e2e087b7bf9f0bf295b0e6a8e3ca073bba33dfd0
Annabelle Zogaib <azogaib@ufl.edu>
2021-10-16 23:49:25 -0400

    Add what was covered in class

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 // Create an Array of at least 3 losing messages
+var losingMessages = [
+    'Better luck next time',
+    'Try again',
+    'Not quite right'
+]
 
 // Create variables to count wins and losses
+var wins = 0
+var losses = 0
 
 // Create variables that target elements with the following IDs: 'message', 'wins', 'losses'
+var messageEl = document.getElementById('message')
+var winsEl = document.getElementById('wins')
+var lossesEl = document.getElementById('losses')
 
 // target all .box elements and attach a click event listener to each one using a loop
+var boxElements = document.querySelectorAll('.box')
+for (var i=0; i < boxElements.length; i++) {
+    var boxElement = boxElements[i];
+    boxElement.onclick = function(event) {
+        // within each click event...
+        // determine which box was clicked with 'this.textContent' or event.target.textContent
+        // convert that value to a Number and store it to a variable
+        var boxNumWasClicked = Number(event.target.textContent)
 
-// within each click event...
-// determine which box was clicked with 'this.textContent' or event.target.textContent
-// convert that value to a Number and store it to a variable
+        // create a random number between 1-3 and store it to a variable
+        // This number will represent the winning box
 
-// create a random number between 1-3 and store it to a variable
-// This number will represent the winning box
 
-// determine if the box clicked is equal to the random number
-// if the numbers match, display a winning message by changing the text content of the div#message element
-// if the numbers match, increment wins and display the win count in div#wins
+        // determine if the box clicked is equal to the random number
+        // if the numbers match, display a winning message by changing the text content of the div#message element
+        // if the numbers match, increment wins and display the win count in div#wins
 
-// if the numbers don't match, change the div#message element's text to a random losing message from the array above
-// if the numbers don't match, increment losses and display the loss count in div#losses
+
+        // if the numbers don't match, change the div#message element's text to a random losing message from the array above
+        // if the numbers don't match, increment losses and display the loss count in div#losses
+
+        
+    }
+}
