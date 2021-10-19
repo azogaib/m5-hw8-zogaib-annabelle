d4db1b53ade07c1e4cc3fbf54f89d94426e1308d
Annabelle Zogaib <azogaib@ufl.edu>
2021-10-18 21:40:33 -0400

    Add +1 variable to fix error

--- index.js
+++ index.js
@@ -19,25 +19,23 @@ var boxElements = document.querySelectorAll('.box')
 for (var i=0; i < boxElements.length; i++) {
     var boxElement = boxElements[i];
     boxElement.onclick = function(event) {
+
         // within each click event...
         // determine which box was clicked with 'this.textContent' or event.target.textContent
         // convert that value to a Number and store it to a variable
         var boxNumWasClicked = Number(event.target.textContent)
 
         // create a random number between 1-3 and store it to a variable
-       // var winningNum = getRandomArbitrary
         // This number will represent the winning box
-
-        var winningBox = 1 + Math.floor(Math.random()*boxElements.length)
-
-        console.log(winningBox)
-        console.log(boxNumWasClicked)
+        var winningBox = Math.floor(Math.random()*boxElements.length)
+        var winningBoxNum = 1 + winningBox
+        
         // determine if the box clicked is equal to the random number
         // if the numbers match, display a winning message by changing the text content of the div#message element
         // if the numbers match, increment wins and display the win count in div#wins
         // if the numbers don't match, change the div#message element's text to a random losing message from the array above
         // if the numbers don't match, increment losses and display the loss count in div#losses
-        if (boxNumWasClicked === winningBox) {
+        if (boxNumWasClicked === winningBoxNum) {
             document.getElementById('message').innerHTML = 'Nice job!';
             wins++
             document.getElementById('wins').innerHTML = 'Wins: ' + wins;
